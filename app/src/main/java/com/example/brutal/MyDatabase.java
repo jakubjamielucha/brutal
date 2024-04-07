18b06b7614d14a5eb6fa848648acf31038414b4c
jakubjamielucha <jakubjamielucha@gmail.com>
2024-04-07 14:23:25 +0200

    Changing adapter and information display.

--- app/src/main/java/com/example/brutal/MyDatabase.java
+++ app/src/main/java/com/example/brutal/MyDatabase.java
@@ -28,8 +28,6 @@ public class MyDatabase extends SQLiteAssetHelper {
                 BuildingModel building = new BuildingModel(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3)
                         , cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), cursor.getString(8), cursor.getString(9)
                         , cursor.getString(10), cursor.getString(11), cursor.getString(12), cursor.getString(11), cursor.getString(12));
-  //                Log.w("DBD", cursor.getString(0));
-  //              Log.w("DBD", cursor.getString(2));
                 buildings.add(building);
             }
             cursor.close();
